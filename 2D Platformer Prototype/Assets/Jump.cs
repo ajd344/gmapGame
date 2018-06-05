b42b2e864e5dfb635c2286c977ae7a8f4ae8fab9
yunatatski <yunatatski@hotmail.com>
2018-06-05 15:42:22 -0400

    added parallaxing environment and demo level updates

--- 2D Platformer Prototype/Assets/Jump.cs
+++ 2D Platformer Prototype/Assets/Jump.cs
@@ -36,7 +36,7 @@ public class Jump : MonoBehaviour {
 
         Vector2 position = transform.position;
         Vector2 direction = Vector2.down;
-        float distance = 1.70f;
+        float distance = .95f;
 
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
         if (hit.collider != null)
